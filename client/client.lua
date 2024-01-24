e356b668c78244b91cf3f1434c09e16421755a50
Atomik <138856192+atomikk0@users.noreply.github.com>
2024-01-24 18:00:58 +0300

    https://discord.gg/GSeQ747HsC

--- client/client.lua
+++ client/client.lua
@@ -2,415 +2,102 @@ local QBCore = exports['qb-core']:GetCoreObject()
 
 
 CreateThread(function()
-    local blip = AddBlipForCoord(1755.09, -1649.48, 111.618)
-    SetBlipSprite(blip, 431)
-    SetBlipDisplay(blip, 4)
-    SetBlipScale(blip, 0.7)
-    SetBlipAsShortRange(blip, true)
-    SetBlipColour(blip, 5)
-    BeginTextCommandSetBlipName('STRING')
-    AddTextComponentSubstringPlayerName("Toptancı")
-    EndTextCommandSetBlipName(blip)
+    for _,v in pairs(Config.Blips) do
+        local blip = AddBlipForCoord(v.coords.x, v.coords.y, v.coords.z)
+        SetBlipSprite(blip, v.sprite)
+        SetBlipDisplay(blip, 4)
+        SetBlipScale(blip, v.scale)
+        SetBlipAsShortRange(blip, true)
+        SetBlipColour(blip, v.color)
+        BeginTextCommandSetBlipName('STRING')
+        AddTextComponentSubstringPlayerName(v.text)
+        EndTextCommandSetBlipName(blip)
+    end
 end)
 
-CreateThread(function(data)
-    exports['qb-target']:AddCircleZone("Toptanci", vector3(1755.09, -1649.48, 111.618), 0.4, { 
-      name = "Toptanci", 
-      debugPoly = false,
-    }, {
-      options = {
-          {
-              type = "client",
-              event = "atomik-npcsatis:toptanci",
-              icon = "fas fa-circle",
-              label = "Toptancı",
-          },
-      },
-      distance = 4.0
-    })
-end)
-
-CreateThread(function(data)
-    exports['qb-target']:AddCircleZone("illegalToptanci", vector3(-1903.6, -573.0, 22.97), 0.4, { 
-      name = "illegalToptanci", 
-      debugPoly = false,
-    }, {
-      options = {
-          {
-              type = "client",
-              event = "atomik-npcsatis:illegaltoptanci",
-              icon = "fas fa-circle",
-              label = "İllegal Toptancı",
-          },
-      },
-      distance = 4.0
-    })
-end)
-
-CreateThread(function(data)
-    exports['qb-target']:AddCircleZone("illegalTacir", vector3(485.35, -3383.72, 6.07), 0.4, { 
-      name = "illegalTacir", 
-      debugPoly = false,
-    }, {
-      options = {
-          {
-              type = "client",
-              event = "atomik-npcsatis:illegaltacir",
-              icon = "fas fa-circle",
-              label = "İllegal Tacir",
-          },
-      },
-      distance = 4.0
-    })
+CreateThread(function()
+    for _,v in pairs(Config.Target) do
+        if v.usejob == true then
+            exports['qb-target']:AddCircleZone(v.label, vector3(v.coords.x, v.coords.y, v.coords.z), 0.4, { 
+                name = v.label, 
+                debugPoly = false,
+              }, {
+                options = {
+                    {
+                        type = v.type,
+                        event = v.event,
+                        icon = v.icon,
+                        label = v.text,
+                        job = v.job,
+                    },
+                },
+                distance = v.distance,
+            })
+        else
+            exports['qb-target']:AddCircleZone(v.label, vector3(v.coords.x, v.coords.y, v.coords.z), 0.4, { 
+                name = v.label, 
+                debugPoly = false,
+              }, {
+                options = {
+                    {
+                        type = v.type,
+                        event = v.event,
+                        icon = v.icon,
+                        label = v.text,
+                    },
+                },
+                distance = v.distance,
+            })
+        end
+    end
 end)
 
 Citizen.CreateThread(function()
-
-    local ped_hash2 = 0x94562DD7 --Ped Hash Giriniz https://wiki.rage.mp/index.php?title=Peds
+    for _,v in pairs(Config.Peds) do
+        local ped_hash2 = v.ped --Ped Hash Giriniz https://wiki.rage.mp/index.php?title=Peds
     
-    RequestModel(ped_hash2)
-    while not HasModelLoaded(ped_hash2) do
-        Wait(1)
+        RequestModel(ped_hash2)
+        while not HasModelLoaded(ped_hash2) do
+            Wait(1)
+        end
+        
+        ped_info2 = CreatePed(1, ped_hash2, v.coords.x, v.coords.y, v.coords.z, v.heading, false, true)
+        SetBlockingOfNonTemporaryEvents(ped_info2, true) 
+        SetPedDiesWhenInjured(ped_info2, false) 
+        SetPedCanPlayAmbientAnims(ped_info2, true) 
+        SetPedCanRagdollFromPlayerImpact(ped_info2, false) 
+        SetEntityInvincible(ped_info2, true)    
+        FreezeEntityPosition(ped_info2, true) 
     end
-    
-    ped_info2 = CreatePed(1, ped_hash2, 1755.09, -1649.48, 111.618, 225.4625549316406, false, true)
-    SetBlockingOfNonTemporaryEvents(ped_info2, true) 
-    SetPedDiesWhenInjured(ped_info2, false) 
-    SetPedCanPlayAmbientAnims(ped_info2, true) 
-    SetPedCanRagdollFromPlayerImpact(ped_info2, false) 
-    SetEntityInvincible(ped_info2, true)    
-    FreezeEntityPosition(ped_info2, true) 
 end)
 
-Citizen.CreateThread(function()
-
-    local ped_hash2 = 0xB594F5C3 --Ped Hash Giriniz https://wiki.rage.mp/index.php?title=Peds
-    
-    RequestModel(ped_hash2)
-    while not HasModelLoaded(ped_hash2) do
-        Wait(1)
-    end
-    
-    ped_info2 = CreatePed(1, ped_hash2, -1903.6, -573.0, 21.97, 220.4625549316406, false, true)
-    SetBlockingOfNonTemporaryEvents(ped_info2, true) 
-    SetPedDiesWhenInjured(ped_info2, false) 
-    SetPedCanPlayAmbientAnims(ped_info2, true) 
-    SetPedCanRagdollFromPlayerImpact(ped_info2, false) 
-    SetEntityInvincible(ped_info2, true)    
-    FreezeEntityPosition(ped_info2, true) 
-end)
-
-Citizen.CreateThread(function()
-
-    local ped_hash2 = 0xF0EC56E2 --Ped Hash Giriniz https://wiki.rage.mp/index.php?title=Peds
-    
-    RequestModel(ped_hash2)
-    while not HasModelLoaded(ped_hash2) do
-        Wait(1)
-    end
-    
-    ped_info2 = CreatePed(1, ped_hash2, 485.35, -3383.72, 5.07, 0, false, true)
-    SetBlockingOfNonTemporaryEvents(ped_info2, true) 
-    SetPedDiesWhenInjured(ped_info2, false) 
-    SetPedCanPlayAmbientAnims(ped_info2, true) 
-    SetPedCanRagdollFromPlayerImpact(ped_info2, false) 
-    SetEntityInvincible(ped_info2, true)    
-    FreezeEntityPosition(ped_info2, true) 
-end)
-
-RegisterNetEvent('atomik-npcsatis:toptanci', function()
+for _, v in pairs(Config.Shops) do
+    RegisterNetEvent(v.event, function()
         local toptanci = {
             {
-                header = "Toptancı",
+                header = v.header,
                 isMenuHeader = true,
             },
-            {
-                header = "Üzüm (30$)",
-                txt = "",
-                params = {
-                    event = 'atomik-npcsatis:adetsor',
-                    args = {
-                        itemName = "grape",
-                        price = 30
-                    }
-                }
-            },
-            {
-                header = "Kereste (15$)",
-                txt = "",
-                params = {
-                    event = 'atomik-npcsatis:adetsor',
-                    args = {
-                        itemName = "kereste",
-                        price = 15
-                    }
-                }
-            },
-            {
-                header = "Paketlenmiş Tavuk (30$)",
-                txt = "",
-                params = {
-                    event = 'atomik-npcsatis:adetsor',
-                    args = {
-                        itemName = "packaged_chicken",
-                        price = 30
-                    }
-                }
-            },
-            {
-                header = "Çelik (5$)",
-                txt = "",
-                params = {
-                    event = 'atomik-npcsatis:adetsor',
-                    args = {
-                        itemName = "steel",
-                        price = 5
-                    }
-                }
-            },
-            {
-                header = "Kauçuk (5$)",
-                txt = "",
-                params = {
-                    event = 'atomik-npcsatis:adetsor',
-                    args = {
-                        itemName = "rubber",
-                        price = 5
-                    }
-                }
-            },
-            {
-                header = "Bakır (5$)",
-                txt = "",
-                params = {
-                    event = 'atomik-npcsatis:adetsor',
-                    args = {
-                        itemName = "copper",
-                        price = 5
-                    }
-                }
-            },
-            {
-                header = "Cam (5$)",
-                txt = "",
-                params = {
-                    event = 'atomik-npcsatis:adetsor',
-                    args = {
-                        itemName = "glass",
-                        price = 5
-                    }
-                }
-            },
-            {
-                header = "Plastik (5$)",
-                txt = "",
-                params = {
-                    event = 'atomik-npcsatis:adetsor',
-                    args = {
-                        itemName = "plastic",
-                        price = 5
-                    }
-                }
-            },
-            {
-                header = "Demir (5$)",
-                txt = "",
-                params = {
-                    event = 'atomik-npcsatis:adetsor',
-                    args = {
-                        itemName = "iron",
-                        price = 5
-                    }
-                }
-            },
-            {
-                header = "Metal Hurda (5$)",
-                txt = "",
-                params = {
-                    event = 'atomik-npcsatis:adetsor',
-                    args = {
-                        itemName = "metalscrap",
-                        price = 5
-                    }
-                }
-            },
-            {
-                header = "Süt (30$)",
-                txt = "",
-                params = {
-                    event = 'atomik-npcsatis:adetsor',
-                    args = {
-                        itemName = "milk",
-                        price = 30
-                    }
-                }
-            },
-            {
-                header = "Mısır (5$)",
-                txt = "",
-                params = {
-                    event = 'atomik-npcsatis:adetsor',
-                    args = {
-                        itemName = "corncob",
-                        price = 5
-                    }
-                }
-            }
         }
+
+        for _, item in pairs(v.items) do
+            local menuItem = {
+                header = item.label,
+                txt = item.description,
+                params = {
+                    event = item.event,
+                    args = {
+                        itemName = item.itemName,
+                        price = item.price,
+                    }
+                }
+            }
+            table.insert(toptanci, menuItem)
+        end
+
         exports['qb-menu']:openMenu(toptanci)
-end)
-
-RegisterNetEvent('atomik-npcsatis:illegaltoptanci', function()
-    local toptanci = {
-        {
-            header = "İllegal Toptancı",
-            isMenuHeader = true,
-        },
-        {
-            header = "Elmas Yüzük (1000$)",
-            txt = "",
-            params = {
-                event = 'atomik-npcsatis:adetsor',
-                args = {
-                    itemName = "diamond_ring",
-                    price = 1000
-                }
-            }
-        },
-        {
-            header = "Elmas (1000$)",
-            txt = "",
-            params = {
-                event = 'atomik-npcsatis:adetsor',
-                args = {
-                    itemName = "diamond",
-                    price = 1000
-                }
-            }
-        },
-    }
-    exports['qb-menu']:openMenu(toptanci)
-end)
-
-RegisterNetEvent('atomik-npcsatis:illegaltacir', function()
-    local toptanci = {
-        {
-            header = "İllegal Tacir",
-            isMenuHeader = true,
-        },
-        {
-            header = "Paketlenmiş Kokain * (60$)",
-            txt = "",
-            params = {
-                event = 'atomik-npcsatis:adetsor',
-                args = {
-                    itemName = "package-coke-bad-ql",
-                    price = 60
-                }
-            }
-        },
-        {
-            header = "Paketlenmiş Kokain ** (70$)",
-            txt = "",
-            params = {
-                event = 'atomik-npcsatis:adetsor',
-                args = {
-                    itemName = "package-coke-med-ql",
-                    price = 70
-                }
-            }
-        },
-        {
-            header = "Paketlenmiş Kokain *** (80$)",
-            txt = "",
-            params = {
-                event = 'atomik-npcsatis:adetsor',
-                args = {
-                    itemName = "package-coke-max-ql",
-                    price = 80
-                }
-            }
-        },
-        {
-            header = "Paketlenmiş Ot * (60$)",
-            txt = "",
-            params = {
-                event = 'atomik-npcsatis:adetsor',
-                args = {
-                    itemName = "package-weed-bad-ql",
-                    price = 60
-                }
-            }
-        },
-        {
-            header = "Paketlenmiş Ot ** (70$)",
-            txt = "",
-            params = {
-                event = 'atomik-npcsatis:adetsor',
-                args = {
-                    itemName = "package-weed-med-ql",
-                    price = 70
-                }
-            }
-        },
-        {
-            header = "Paketlenmiş Ot *** (80$)",
-            txt = "",
-            params = {
-                event = 'atomik-npcsatis:adetsor',
-                args = {
-                    itemName = "package-weed-max-ql",
-                    price = 80
-                }
-            }
-        },
-        {
-            header = "Paketlenmiş Afyon * (60$)",
-            txt = "",
-            params = {
-                event = 'atomik-npcsatis:adetsor',
-                args = {
-                    itemName = "package-opium-bad-ql",
-                    price = 60
-                }
-            }
-        },
-        {
-            header = "Paketlenmiş Afyon ** (70$)",
-            txt = "",
-            params = {
-                event = 'atomik-npcsatis:adetsor',
-                args = {
-                    itemName = "package-opium-med-ql",
-                    price = 70
-                }
-            }
-        },
-        {
-            header = "Paketlenmiş Afyon *** (80$)",
-            txt = "",
-            params = {
-                event = 'atomik-npcsatis:adetsor',
-                args = {
-                    itemName = "package-opium-max-ql",
-                    price = 80
-                }
-            }
-        },
-        {
-            header = "Meth (100$)",
-            txt = "",
-            params = {
-                event = 'atomik-npcsatis:adetsor',
-                args = {
-                    itemName = "meth",
-                    price = 100
-                }
-            }
-        },
-    }
-    exports['qb-menu']:openMenu(toptanci)
-end)
+    end)
+end
 
 RegisterNetEvent("atomik-npcsatis:adetsor", function(data)
     local dialog = exports['qb-input']:ShowInput({
@@ -420,7 +107,6 @@ RegisterNetEvent("atomik-npcsatis:adetsor", function(data)
             { type = 'number', isRequired = true, name = 'price', text = 'Miktar' },
         }
     })
-    local miktar = tonumber(dialog.price)
     if dialog then
         if not dialog.price then return end
         local miktar = tonumber(dialog.price)
@@ -428,4 +114,17 @@ RegisterNetEvent("atomik-npcsatis:adetsor", function(data)
     end
 end)
 
+RegisterNetEvent("atomik-npcsatis:adetsor2", function(data)
+    local dialog = exports['qb-input']:ShowInput({
+        header = "Miktar Gir",
+        submitText = "Sat",
+        inputs = {
+            { type = 'number', isRequired = true, name = 'price', text = 'Miktar' },
+        }
+    })
+    if dialog then
+        if not dialog.price then return end
+        TriggerServerEvent("atomik-npcsatis:sat2", data.itemName , tonumber(dialog.price), data.price)
+    end
+end)
 
